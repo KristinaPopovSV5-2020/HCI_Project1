f875b9f38b9bb95ced5bfcb3a8865882ad519038
KristinaPopovSV5-2020 <99082673+KristinaPopovSV5-2020@users.noreply.github.com>
2023-04-07 00:55:50 +0200

    added weather service

--- weather_app/src/app/app.module.ts
+++ weather_app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './infrastructure/material.module';
 import { WeatherHomeComponent } from './components/weather-home/weather-home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { WeatherHomeComponent } from './components/weather-home/weather-home.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
